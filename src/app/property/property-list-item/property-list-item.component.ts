7ababd9a89cf4992d3bb48588097e830068788b7
Marcus Stockton <marcus_stockton@hotmail.co.uk>
2020-09-07 19:20:57 +0100

    Added property components

---
+++ src/app/property/property-list-item/property-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-list-item',
+  templateUrl: './property-list-item.component.html',
+  styleUrls: ['./property-list-item.component.scss']
+})
+export class PropertyListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/property/property.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PropertyDashboardComponent } from './property-dashboard/property-dashboard.component';
+import { PropertyDetailComponent } from './property-detail/property-detail.component';
+import { PropertyListItemComponent } from './property-list-item/property-list-item.component';
+import { PropertyFormComponent } from './property-form/property-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: PropertyDashboardComponent },
+  { path: 'create', component: PropertyFormComponent },
+  { path: ':id', component: PropertyDetailComponent },
+  { path: ':id/edit', component: PropertyFormComponent }
+];
+
+
+@NgModule({
+  declarations: [
+    PropertyDashboardComponent,
+    PropertyDetailComponent,
+    PropertyListItemComponent,
+    PropertyFormComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    MaterialModule
+  ]
+})
+export class PropertyModule { }
